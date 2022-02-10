478451489fc70bd031987c68f5c302865021777d
Jeremias Tivane <22341964+Jeremias-Tivane@users.noreply.github.com>
2022-02-10 23:24:30 +0200

    LESSON 2
    LESSON 2_SQL Joins.sql

--- SQL for Data Analysis - Udacity/SQL Joins.sql
+++ SQL for Data Analysis - Udacity/SQL Joins.sql
@@ -93,7 +93,7 @@ FROM accounts a
 JOIN orders o
 ON o.account_id = a.id
 WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
-ORDER BY o.occurred_at DESC;
+ORDER BY o.occurred_at DESC; 
 
 
 --======================END (Jeremias Tivane - https://www.linkedin.com/in/jeremiastivane )
